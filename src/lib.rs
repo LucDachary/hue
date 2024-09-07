25c1db72ebfd788a75d76082e9cc3cb7d1b84a65
Luc Dachary <luc@inarix.com>
2024-09-07 18:26:57 +0200

    Adds a few basic functions in library.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,93 @@
+use anyhow::Context;
 use serde_json::Value;
 use std::collections::HashMap;
 
-pub fn list_lights(bridge_ip: &String, bridge_user: &String) -> anyhow::Result<()> {
-    let response =
-        reqwest::blocking::get(&format!("http://{}/api/{}/lights", bridge_ip, bridge_user))?;
+/// List all lights connected to the bridge.
+pub fn list_lights(
+    client: reqwest::blocking::Client,
+    bridge_ip: &String,
+    bridge_user: &String,
+) -> anyhow::Result<()> {
+    let response = client
+        .get(&format!("http://{}/api/{}/lights", bridge_ip, bridge_user))
+        .send()
+        .with_context(|| format!("Failed to build the HTTP request."))?;
 
-    let lights: HashMap<String, Value> = response.json()?;
+    let lights: HashMap<String, Value> = response
+        .json()
+        .with_context(|| "Failed to extract JSON from lights list.")?;
+
     for (id, light) in lights {
         let name = light["name"].as_str().unwrap();
-        println!("{}: {}", id, name);
+        println!("{:>3}: {}", id, name);
     }
+    Ok(())
+}
+
+/// List all groups configured on the bridge.
+pub fn list_groups(
+    client: reqwest::blocking::Client,
+    bridge_ip: &String,
+    bridge_user: &String,
+) -> anyhow::Result<()> {
+    let response = client
+        .get(&format!("http://{}/api/{}/groups", bridge_ip, bridge_user))
+        .send()
+        .with_context(|| format!("Failed to build the HTTP request."))?;
+
+    let groups: HashMap<String, Value> = response
+        .json()
+        .with_context(|| "Failed to extract JSON from groups list.")?;
+
+    for (id, group) in groups {
+        let name = group["name"].as_str().unwrap();
+        println!("{:>3}: {}", id, name);
+    }
+    Ok(())
+}
+
+pub fn turn_on_light(
+    client: reqwest::blocking::Client,
+    bridge_ip: &String,
+    bridge_user: &String,
+    light_id: &String,
+) -> anyhow::Result<()> {
+    let response = client
+        .put(&format!(
+            "http://{}/api/{}/lights/{}/state",
+            bridge_ip, bridge_user, light_id
+        ))
+        .json(&serde_json::json!({"on": true, "bri": 254}))
+        .send()
+        .with_context(|| format!("Failed to build the HTTP request."))?;
+
+    let response_json: Value = response
+        .json()
+        .with_context(|| "Failed to extract JSON from response.")?;
+
+    println!("{}", response_json);
+    Ok(())
+}
+
+pub fn turn_off_light(
+    client: reqwest::blocking::Client,
+    bridge_ip: &String,
+    bridge_user: &String,
+    light_id: &String,
+) -> anyhow::Result<()> {
+    let response = client
+        .put(&format!(
+            "http://{}/api/{}/lights/{}/state",
+            bridge_ip, bridge_user, light_id
+        ))
+        .json(&serde_json::json!({"on": false}))
+        .send()
+        .with_context(|| format!("Failed to build the HTTP request."))?;
+
+    let response_json: Value = response
+        .json()
+        .with_context(|| "Failed to extract JSON from response.")?;
+
+    println!("{}", response_json);
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use anyhow;
-use log::{debug, info};
-
-use hue::list_lights;
+use log::info;
+use reqwest;
+use std::env;
+use std::thread::sleep;
+use std::time;
 
 const BRIDGE_IP: &str = "192.168.1.14";
 const USERNAME: &str = "G5yH6nGxpayxZjOiglI-99WeP5T9N0qvnC4FCEZV";
@@ -9,6 +11,18 @@ const USERNAME: &str = "G5yH6nGxpayxZjOiglI-99WeP5T9N0qvnC4FCEZV";
 fn main() -> anyhow::Result<()> {
     env_logger::init();
     info!("Running the testing program…");
+
+    // Build a client with a proxy, if HTTP_PROXY and HTTPS_PROXY have been set.
+
+    let client: reqwest::blocking::Client = match env::var("HTTP_PROXY") {
+        Ok(http_proxy) => reqwest::blocking::Client::builder()
+            .proxy(reqwest::Proxy::http(http_proxy)?)
+            .build()?,
+        Err(_) => {
+            info!("No proxy set. Returning a regular client.");
+            reqwest::blocking::Client::new()
+        }
+    };
 
     // sudo rustscan -b 200 -r 80-80 --addresses 192.168.1.0/24
     // -> 192.168.1.14
@@ -32,8 +46,37 @@ fn main() -> anyhow::Result<()> {
     // Get only names and state with `jq`:
     // http $HUE/api/$HUE_USER/lights  | jq -r 'pick(.[].name, .[].state.on)'
 
-    debug!("Listing lights…");
-    list_lights(&BRIDGE_IP.to_string(), &USERNAME.to_string())?;
+    info!("Listing lights…");
+    hue::list_lights(
+        client.clone(),
+        &BRIDGE_IP.to_string(),
+        &USERNAME.to_string(),
+    )?;
+
+    info!("Listing groups…");
+    hue::list_groups(
+        client.clone(),
+        &BRIDGE_IP.to_string(),
+        &USERNAME.to_string(),
+    )?;
+
+    info!("Turning on light 1…");
+    hue::turn_on_light(
+        client.clone(),
+        &BRIDGE_IP.to_string(),
+        &USERNAME.to_string(),
+        &"1".to_string(),
+    )?;
+
+    sleep(time::Duration::from_secs(2));
+
+    info!("Turning off light 1…");
+    hue::turn_off_light(
+        client.clone(),
+        &BRIDGE_IP.to_string(),
+        &USERNAME.to_string(),
+        &"1".to_string(),
+    )?;
 
     Ok(())
 }
